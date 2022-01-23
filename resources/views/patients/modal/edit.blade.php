f328138d84f5405c46b50d64357114eff83ac274
JoelOdhiambo <wesleyjoel4@gmail.com>
2022-01-23 12:14:20 +0300

    Final Project Commit

--- resources/views/patients/modal/edit.blade.php
+++ resources/views/patients/modal/edit.blade.php
@@ -12,7 +12,7 @@
                             Patient No: <input type="text" name="patient_id" id="patient_id">
                             <button type="button"
                                 class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                                id="modal-close">
+                                id="modal-close" data-modal-toggle="edit-modal">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
